2fa0a53a593820cf31e2c14dce3f9950b1462ac5
rivva <rivvarivvs@gmail.com>
2020-12-25 16:37:58 +0000

    added mockup of the signup route business logic

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,13 +1,30 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 
 const router = express.Router();
 
-router.post("/api/users/signupRouter", (req, res) => {
+router.post(
+  "/api/users/signup",
+  [
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 6, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  (req: Request, res: Response) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).send(errors.array());
+    }
+
     const { email, password } = req.body;
 
-    if (!email || typeof email !== 'string') {
-        res.status(400).send('Provide a valid email')
-    }
-});
+    console.log("Creating a user...");
+
+    res.send({});
+  }
+);
 
 export { router as signupRouter };
