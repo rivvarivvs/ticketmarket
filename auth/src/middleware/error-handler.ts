388b9ce78d63ad4cb6443f0ed5e60931d720c48e
rivva <rivvarivvs@gmail.com>
2020-12-25 18:24:43 +0000

    did all the error handling basis, including the construction of the abstract class of CustomError

--- auth/src/middleware/error-handler.ts
+++ auth/src/middleware/error-handler.ts
@@ -1,9 +1,17 @@
 import { Request, Response, NextFunction } from "express";
+import { CustomError } from "../errors/custom-error";
 
-(err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.log('Something went wrong', err)
+export const errorHandler = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof CustomError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
 
-    res.status(400).send({
-        message: 'Something went wrong'
-    })
+  res.status(400).send({
+    errors: [{ message: "Something went wrong" }],
+  });
 };
