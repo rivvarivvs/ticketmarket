9f71bbc9d7f2b5b10258831935c1f16b92f2c501
rivva <rivvarivvs@gmail.com>
2021-01-10 17:02:14 +0000

    expanded the tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -2,9 +2,65 @@ import request from 'supertest';
 import { app } from '../../app';
 
 it('returns a 201 on successful signup', async () => {
-	return request(app).post('/api/users/signup').send({
-		email: 'test@test.com',
-		password: 'password',
-    })
-    .expect(201);
+	return request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'test@test.com',
+			password: 'password',
+		})
+		.expect(201);
+});
+
+it('returns a 400 with an invalid email', async () => {
+	return request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'asdasd',
+			password: 'password',
+		})
+		.expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+	return request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'test1@test.com',
+			password: '1',
+		})
+		.expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+	return request(app).post('/api/users/signup').send({}).expect(400);
+});
+
+it('does not allow to register with the same email', async () => {
+	await request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'test1@test.com',
+			password: 'asdfasdf',
+		})
+		.expect(201);
+
+	await request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'test1@test.com',
+			password: 'asdfasdf',
+		})
+		.expect(400);
+});
+
+it('sets a cookie after signing up', async () => {
+	const response = await request(app)
+		.post('/api/users/signup')
+		.send({
+			email: 'test1@test.com',
+			password: 'asdfasdf',
+        })
+        .expect(201);
+
+    expect(response.get('Set-Cookie')).toBeUndefined;
 });
