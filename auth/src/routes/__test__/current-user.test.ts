23ce00eee85be888c8ac7c7bcc4d6f4424817bcf
rivva <rivvarivvs@gmail.com>
2021-01-10 21:01:05 +0000

    finished testin the current auth service. so far only have unit tests

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,24 @@
+import e from 'express';
+import request from 'supertest';
+import { app } from '../../app';
+
+it('responds with details about current user', async () => {
+	const cookie = await global.signin();
+
+	const response = await request(app)
+		.get('/api/users/currentuser')
+		.set('Cookie', cookie)
+		.send()
+		.expect(200);
+
+	expect(response.body.currentUser.email).toEqual('test@test.com');
+});
+
+it('responds with null if not authenticated', async () => {
+	const res = await request(app)
+		.get('/api/users/currentuser')
+		.send()
+		.expect(200);
+
+	expect(res.body.currentUser).toEqual(null);
+});
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,11 +1,21 @@
+import request from 'supertest';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
+
+declare global {
+	namespace NodeJS {
+		interface Global {
+			signin(): Promise<string[]>;
+		}
+	}
+}
 
 let mongo: any;
 
 beforeAll(async () => {
 	process.env.JWT_KEY = 'asdasd';
+	process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 	mongo = new MongoMemoryServer();
 	const mongoUri = await mongo.getUri();
@@ -28,3 +38,20 @@ afterAll(async () => {
 	await mongo.stop();
 	await mongoose.connection.close();
 });
+
+global.signin = async () => {
+	const email = 'test@test.com';
+	const password = 'password';
+
+	const res = await request(app)
+		.post('/api/users/signup')
+		.send({
+			email,
+			password,
+		})
+		.expect(201);
+
+	const cookie = res.get('Set-Cookie');
+
+	return cookie;
+};
