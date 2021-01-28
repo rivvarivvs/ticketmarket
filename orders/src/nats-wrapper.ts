c11e398caf02aaee2deb915db114968d87298092
rivva <rivvarivvs@gmail.com>
2021-01-28 15:23:29 +0000

    added the eventbus publisher to the routes

--- orders/src/nats-wrapper.ts
+++ orders/src/nats-wrapper.ts
@@ -16,8 +16,7 @@ class NatsWrapper {
 
     return new Promise((resolve, reject) => {
       this.client.on('connect', () => {
-        console.log('Connected to NATS');
-        resolve();
+        resolve(console.log('Connected to NATS'));
       });
       this.client.on('error', (err) => {
         reject(err);
